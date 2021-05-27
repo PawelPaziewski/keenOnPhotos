a6508d8f170c43fdf52f4e0ab8db15f68abe7a09
PawelPaziewski <lrpapawel@gmail.com>
2021-05-27 21:06:36 +0200

    Initial commit

---
+++ src/main/java/pl/paziewski/keenonphotos/KeenOnPhotosApplication.java
@@ -0,0 +1,12 @@
+package pl.paziewski.keenonphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeenOnPhotosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeenOnPhotosApplication.class, args);
+    }
+}
