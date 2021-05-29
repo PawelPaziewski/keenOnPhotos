0caeeb24755a0038f0c779a28e1309a43dd19cfc
PawelPaziewski <lrpapawel@gmail.com>
2021-05-29 21:55:46 +0200

    Configure security mappings

--- src/main/java/pl/paziewski/keenonphotos/home/HomeController.java
+++ src/main/java/pl/paziewski/keenonphotos/home/HomeController.java
@@ -5,15 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-import java.util.Arrays;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    String home(Principal principal, Model model){
+    @GetMapping(value = {"/", "/home"})
+    String home(Principal principal, Model model) {
         return "home";
     }
-
-
 }
--- src/main/java/pl/paziewski/keenonphotos/security/SecurityConfig.java
+++ src/main/java/pl/paziewski/keenonphotos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,6 +17,17 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     SecurityConfig(MongoUserDetailService mongoUserDetailService) {
         this.mongoUserDetailService = mongoUserDetailService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").permitAll()
+                .anyRequest().authenticated();
+        http.logout()
+                .logoutSuccessUrl("/home");
+        http.formLogin();
     }
 
     @Bean
