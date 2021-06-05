fb255591dbf1c1fcdf31642cd0afad878a6bad10
PawelPaziewski <lrpapawel@gmail.com>
2021-06-05 11:55:59 +0200

    Add photo resizing

--- src/main/java/pl/paziewski/keenonphotos/photos/PhotoHelper.java
+++ src/main/java/pl/paziewski/keenonphotos/photos/PhotoHelper.java
@@ -1,22 +1,37 @@
 package pl.paziewski.keenonphotos.photos;
 
+import org.imgscalr.Scalr;
 import org.joda.time.LocalDateTime;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 
 class PhotoHelper {
 
+    private static final int TARGET_SIZE = 1920;
+
     Photo mapToPhoto(PhotoDto dto) throws IOException {
         return new Photo(
                 dto.getTitle(),
                 dto.getOwnerUsername(),
-                dto.getPhoto().getBytes(),
+                resizeAndMapToBytes(dto.getPhoto()),
                 dto.getDescription(),
                 LocalDateTime.now(),
                 dto.getCameraParameters(),
                 dto.getLocation()
         );
+    }
+
+    private byte[] resizeAndMapToBytes(MultipartFile photo) throws IOException {
+        BufferedImage buffered = ImageIO.read(photo.getInputStream());
+        BufferedImage resized = Scalr.resize(buffered, TARGET_SIZE);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(resized, "png", byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
     }
 
     static LatestPhotoDto convertToLatestPhotoDto(Photo photo) {
